41f82fbedf5bdaf3de65779071e894ababbc8c17
jiyoung-k <jiyoungkim@neuronaware.com>
2020-09-25 16:51:28 +0900

    wpf mvvm sample

---
+++ WPFMVVM/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFMVVM
+{
+    public class Command : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public Command(Action<object> execute, Func<object,bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ WPFMVVM/CurrencyConverterViewModel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFMVVM
+{
+    public class Currency
+    {
+        public string Title { get; set; }
+        public decimal Rate { get; set; }
+
+        public Currency(string title, decimal rate)
+        {
+            Title = title;
+            Rate = rate;
+        }
+    }
+
+
+    public class CurrencyConverterViewModel:Notifier
+    {
+        public ICommand MyCommand { get; set; }
+
+        private bool CanExecute(object arg)
+        {
+            return true;
+        }
+
+        private void Execute(object obj)
+        {
+            MessageBox.Show("use ICommand !!!");
+        }
+
+        private decimal euros;
+
+        public decimal Euros
+        {
+            get { return euros; }
+            set
+            {
+                euros = value;
+                OnPropertyChanged("Euros");
+                OnEurosChanged();
+            }
+        }
+
+        private decimal converted;
+
+        public decimal Converted
+        {
+            get { return converted; }
+            set
+            {
+                converted = value;
+                OnPropertyChanged("Converted");
+            }
+        }
+
+        private Currency selectedCurrency;
+
+        public Currency SelectedCurrency
+        {
+            get { return selectedCurrency; }
+            set
+            {
+                selectedCurrency = value;
+                OnPropertyChanged("SelectedCurrency");
+                OnSelectedCurrencyChanged();
+            }
+        }
+
+        private IEnumerable<Currency> currencies;
+
+        public IEnumerable<Currency> Currencies
+        {
+            get { return currencies; }
+            set
+            {
+                currencies = value;
+                OnPropertyChanged("Currencies");
+            }
+        }
+
+        private string resultText;
+
+        public string ResultText
+        {
+            get { return resultText; }
+            set
+            {
+                resultText = value;
+                OnPropertyChanged("ResultText");
+            }
+        }
+
+        private string resultRate;
+
+        public string ResultRate
+        {
+            get { return resultRate; }
+            set
+            {
+                resultRate = value;
+                OnPropertyChanged("ResultRate");
+            }
+        }
+
+
+        public CurrencyConverterViewModel()
+        {
+            MyCommand = new Command(Execute, CanExecute);
+
+            Currencies = new Currency[]
+            {
+                new Currency("US Dollar", 1.1M),
+                new Currency("British Pound", 0.9M)
+            };
+        }
+
+        private void OnEurosChanged()
+        {
+            ComputeConverted();
+        }
+
+        private void OnSelectedCurrencyChanged()
+        {
+            ComputeConverted();
+        }
+
+        private void ComputeConverted()
+        {
+            if (SelectedCurrency == null)
+            {
+                return;
+            }
+
+            Converted = Euros * SelectedCurrency.Rate;
+            ResultText = string.Format("Amount in {0}", selectedCurrency.Title);
+            ResultRate = string.Format("Rate is {0}", selectedCurrency.Rate);
+        }
+
+
+        
+
+    }
+}
